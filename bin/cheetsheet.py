350cf7b44f1eca79705538a61c12ca374b4ce4ca
Mark McCoy <mark@mccoyfam.net>
2014-02-14 16:32:44 -0600

    working version, basic layout

--- bin/cheetsheet.py
+++ bin/cheetsheet.py
@@ -18,11 +18,91 @@ along with Cheetsheet.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+import os
+import sys
+
+
+def buildPDF(data, filename):
+	""" create a PDF document"""
+	doc = SimpleDocTemplate(
+		filename, 
+		pagesize=letter,
+		rightMargin=36,leftMargin=36,
+		topMargin=36,bottomMargin=18
+	)
+
+	Story = []  # this will be the text of the document that is flowed into the PDF
+
+	styles = getSampleStyleSheet()
+	styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+
+	# add the header to the Story
+	header = 'Title: <b>%s</b><br/>\
+	Artist: <b>%s</b><br/>\
+	Key: <b>%s</b><br/>\
+	Tempo: <b>%s</b><br/>' % (
+		data.get('title'), 
+		data.get('artist'), 
+		data.get('key'), 
+		data.get('tempo'))
+	Story.append(Paragraph(header, styles['Normal']))
+	Story.append(Spacer(1,12))
+
+	for section in data.get('sections'):
+		notestr = ''
+		if section.get('notes'):
+			for note in section.get('notes'):
+				notestr += "- %s<br/>" % note
+
+		lyrstr = ''
+		if section.get('lyrics'):
+			#lyrstr += '<para leftIndent="20">'
+			for lyric in section.get('lyrics'):
+				lyrstr += "%s<br/>" % lyric 
+			#lyrstr += "</para>"
+
+		para = """
+		<b>%s</b><br/>
+		<i>- %s measure%s<br/>
+		%s</i>
+		%s
+		""" % (
+			section.get('title'), 
+			section.get('measures', "0"),
+			's' if section.get('measures', None) else '',
+			notestr,
+			lyrstr
+		)
+		Story.append(Paragraph(para, styles['Normal']))
+		#Story.append(Paragraph(lyrstr, styles['Normal']))
+		Story.append(Spacer(1,12))
+
+	# create the PDF and write it out
+	doc.build(Story)
+
+
+
+
+
 
 def main(args):
-	"""Main loop, process each file in turn"""
+	"""Main loop, import the file, parse out the json, process it, and output the PDF"""
+	import json
 	for filename in args.input:
-		print "processing %s" % (filename)
+		(outfile, ext) = os.path.splitext(filename)
+		outfile = outfile + ".pdf"
+
+		print "processing %s, writing to %s" % (filename,outfile)
+		raw = open(filename).read()
+		jdata = json.loads(raw)
+
+		doc = buildPDF(jdata, outfile)
 
 
 if __name__ == "__main__":
